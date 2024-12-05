05e38e151eeef195a43496650002926418e7cdce
Gdhdh Likry <ralnikov08@gmail.com>
2024-12-05 20:35:02 +0300

    auth & tasks ready working

--- src/main/java/com/example/taskmanager/TaskmanagerApplication.java
+++ src/main/java/com/example/taskmanager/TaskmanagerApplication.java
@@ -2,8 +2,12 @@ package com.example.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.taskmanager.repositories")
+@EntityScan(basePackages = "com.example.taskmanager.models")
 public class TaskmanagerApplication {
 
 	public static void main(String[] args) {
